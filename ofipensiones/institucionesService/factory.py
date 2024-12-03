118f20f669f2a80580e355b578626b0ec7ab4b85
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-03 12:03:43 -0500

    cambio en la generación de instituciones

--- ofipensiones/institucionesService/factory.py
+++ ofipensiones/institucionesService/factory.py
@@ -12,6 +12,7 @@ class InstitucionFactory(MongoEngineFactory):
 
     factory.Faker.add_provider(SchoolProvider)
     nombreInstitucion = Faker('school_name')
+
     # Usamos LazyAttribute para crear los cursos para cada institución
     cursos = factory.LazyAttribute(lambda o: [
         CursoFactory(grado=grado, numero=numero, anio=2024)
@@ -19,18 +20,17 @@ class InstitucionFactory(MongoEngineFactory):
         for numero in [1, 2]
     ])
 
-
 class CursoFactory(MongoEngineFactory):
     class Meta:
         model = Curso
 
-    id = ObjectId()
+    # Usamos ObjectId() para garantizar que cada curso tenga un id único
+    id = factory.LazyFunction(ObjectId)
     grado = factory.Iterator(
-        ['Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto', 'Sexto', 'Séptimo', 'Octavo', 'Noveno', 'Décimo',
-         'Undécimo'])
+        ['Primero', 'Segundo', 'Tercero', 'Cuarto', 'Quinto', 'Sexto', 'Séptimo', 'Octavo', 'Noveno', 'Décimo', 'Undécimo']
+    )
     numero = factory.Iterator([1, 2])  # Solo números 1 y 2
     anio = 2024  # Año fijo 2024
-
 
 # Crear instituciones con cursos
 def crear_instituciones_con_cursos():
