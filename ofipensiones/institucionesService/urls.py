ada0b5c37155ae9e3494c5954e1269be432e7270
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-02 19:01:40 -0500

    urls

--- ofipensiones/institucionesService/urls.py
+++ ofipensiones/institucionesService/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('crear-institucion/', views.post, name='crear_institucion'),
-    path('listar-instituciones/', views.get, name='listar_instituciones'),
-    path('eliminar-instituciones/', views.delete, name='eliminar_instituciones')
+    path('instituciones/crear-institucion/', views.post, name='crear_institucion'),
+    path('instituciones/listar-instituciones/', views.get, name='listar_instituciones'),
+    path('instituciones/eliminar-instituciones/', views.delete, name='eliminar_instituciones')
 ]
