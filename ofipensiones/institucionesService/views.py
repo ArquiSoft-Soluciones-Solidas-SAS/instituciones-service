cb56c4a27c80a45cafedb74fe858ee3b74965e3d
KevinVel16 <90013072+kevinevelasco@users.noreply.github.com>
2024-12-05 11:16:49 -0500

    cqrs prueba

--- ofipensiones/institucionesService/views.py
+++ ofipensiones/institucionesService/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from .models import Institucion, Curso
+from .utils import send_to_rabbitmq
 
 @csrf_exempt
 def post(request):
@@ -10,13 +11,29 @@ def post(request):
         curso1 = Curso(id=ObjectId(), grado="Primero", numero=1, anio=2024)
         curso2 = Curso(id=ObjectId(), grado="Segundo", numero=2, anio=2024)
 
-
-
         institucion = Institucion(
             nombreInstitucion="Institución de Prueba",
             cursos=[curso1, curso2]
         )
         institucion.save()
+        send_to_rabbitmq(
+            exchange='instituciones',
+            routing_key='institucion.created',
+            message={
+                "type": "institucion_created",
+                "data": {
+                    "id": str(institucion.id),
+                    "nombreInstitucion": institucion.nombreInstitucion,
+                    "cursos": [
+                        {"id": str(curso.id),
+                         "grado": curso.grado,
+                         "numero": curso.numero,
+                         "anio": curso.anio}
+                        for curso in institucion.cursos
+                    ]
+                }
+            }
+        )
 
         return JsonResponse({
             "mensaje": "Institución creada exitosamente",
